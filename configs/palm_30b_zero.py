b35e52c39b7f452d780839ce19ec6aca246d09b8
ver217 <lhx0217@gmail.com>
2022-04-18 20:16:53 +0800

    add palm 30b zero config (#37)
    * add palm 30b zero config
    
    * polish code

---
+++ configs/palm_30b_zero.py
@@ -0,0 +1,24 @@
+from colossalai.zero.shard_utils import TensorShardStrategy
+
+SEQ_LENGTH = 2048
+BATCH_SIZE = 4
+NUM_EPOCHS = 1
+
+
+model = dict(
+    type="palm_30b",
+    use_grad_checkpoint=True,
+    use_act_offload=False,
+)
+
+zero = dict(
+    model_config=dict(
+        shard_strategy=TensorShardStrategy(),
+        tensor_placement_policy='cuda',
+    ),
+    optimizer_config=dict(
+        gpu_margin_mem_ratio = 0.9,
+        initial_scale=2**5,
+    )
+)
+
