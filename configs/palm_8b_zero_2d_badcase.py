50ecdb50399e8d91f140c4e2255e81273343095b
jiaruifang <fangjiarui123@gmail.com>
2022-04-19 17:13:43 +0800

    add bad case configs

---
+++ configs/palm_8b_zero_2d_badcase.py
@@ -0,0 +1,28 @@
+from colossalai.zero.shard_utils import TensorShardStrategy
+
+SEQ_LENGTH = 2048
+BATCH_SIZE = 16 
+NUM_EPOCHS = 1
+# WARMUP_EPOCHS = 1
+
+parallel = dict(
+    tensor=dict(mode="2d", size=4),
+)
+
+model = dict(
+    type="palm_8b",
+    use_grad_checkpoint=True,
+    use_act_offload=False,
+)
+
+zero = dict(
+    model_config=dict(
+        shard_strategy=TensorShardStrategy(),
+        tensor_placement_policy='cpu',
+    ),
+    optimizer_config=dict(
+        gpu_margin_mem_ratio = 0.8,
+        initial_scale=2**5,
+    )
+)
+
