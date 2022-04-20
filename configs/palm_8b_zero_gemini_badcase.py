38d8111f102e2c6163d3303ff97b9b867fba3b0f
jiaruifang <fangjiarui123@gmail.com>
2022-04-20 10:27:37 +0800

    add gemini 8b badcase

---
+++ configs/palm_8b_zero_gemini_badcase.py
@@ -0,0 +1,31 @@
+from colossalai.zero.shard_utils import TensorShardStrategy
+
+SEQ_LENGTH = 2048
+BATCH_SIZE = 16 
+NUM_EPOCHS = 1
+# WARMUP_EPOCHS = 1
+
+parallel = dict(
+    tensor=dict(mode="1d", size=2),
+)
+
+model = dict(
+    type="palm_8b",
+    use_grad_checkpoint=True,
+    use_act_offload=False,
+)
+
+
+# auto will fail
+# cpu will pass
+zero = dict(
+    model_config=dict(
+        shard_strategy=TensorShardStrategy(),
+        tensor_placement_policy='cpu',
+    ),
+    optimizer_config=dict(
+        gpu_margin_mem_ratio = 0.8,
+        initial_scale=2**5,
+    )
+)
+
